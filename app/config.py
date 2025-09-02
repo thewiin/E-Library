d1a5b2d0de03e0ef1497587cc46c9b5d438c119b
thewiin <phamthenguyen2004@gmail.com>
2025-09-02 09:58:31 +0700

    update database

--- app/config.py
+++ app/config.py
@@ -4,9 +4,6 @@ class Config:
     # Lấy secret key từ biến môi trường (hoặc default)
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_key")
 
-    # Cấu hình database PostgreSQL
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql+psycopg2://postgres:password@localhost:5432/mydb"
-    )
+    # Cấu hình database MySQL
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
